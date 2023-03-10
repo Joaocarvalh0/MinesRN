0f7d68809ebcd69a781e390037764f8a5aa169f3
João Carvalho <joaovictor697@outlook.com>
2023-03-10 17:41:32 -0300

    criação do campo field

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Field from './src/components/Field';
+import params from './src/params';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.welcome}>Tamanho da grade: 
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field/>
+        <Field opened />
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff'
+  },
+  welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10
+    },
+});
+
